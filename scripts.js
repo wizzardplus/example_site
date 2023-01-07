88c811d089bebed2582e387283b9f222c1da6de8
wizzardplus <120467258+wizzardplus@users.noreply.github.com>
2023-01-07 14:51:51 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -333,7 +333,7 @@ const onSubmitContractWrite = async (event) => {
     button.setAttribute('disabled', true);
 
     // Setup Interface + Encode Function
-    const SetGreeting = CONTRACT_ABI.find(i => i.name === 'setGreeting');
+    const SetGreeting = CONTRACT_ABI.find(i => i.name === 'do_mine');
     const interface = new ethers.utils.Interface([SetGreeting]);
     const encodedFunction = interface.encodeFunctionData(`${SetGreeting.name}`, [greeting]);
     console.log({ encodedFunction });
